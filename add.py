7b59880738678f864313cf0ca0cce31a8aaff0af
CarolinePriyaDarshini <carlynepriya@gmail.com>
2018-01-22 02:20:07 -0800

    add.py

---
+++ add.py
@@ -0,0 +1,10 @@
+# Simple Python program to Add Two Numbers
+ 
+number1 = input(" Please Enter the First Number: ")
+number2 = input(" Please Enter the second number: ")
+number3 = input(" Please Enter the third number: ")
+# Using arithmetic + Operator to add two numbers
+sum = float(number1) + \
+      float(number2) + \
+      int(number3)
+print('The sum of {0},{1} and {2} is {3}'.format(number1, number2,number3, sum))
